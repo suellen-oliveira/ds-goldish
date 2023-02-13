a8f4654d0ed730e558ea96cac2f872cc35da39a1
Suellen Oliveira <dev.suellen@gmail.com>
2023-02-13 19:22:39 -0300

    add props ao botao

--- src/components/AbButton/index.tsx
+++ src/components/AbButton/index.tsx
@@ -1,19 +1,45 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const AbButton = () => {
-  return <Button>Clique aqui</Button>;
+export interface AbButtonProps {
+  text?: string;
+  buttonType?: 'primary' | 'secondary';
+  onClick?: () => void;
+}
+
+export const AbButton: React.FC<AbButtonProps> = ({
+  onClick,
+  text,
+  buttonType = 'primary',
+}) => {
+  return (
+    <Button onClick={onClick} buttonType={buttonType}>
+      {text}
+    </Button>
+  );
 };
 
-const Button = styled.button`
-  background: #eb9b00;
+const Button = styled.button<AbButtonProps>`
+  background: ${({ buttonType }) =>
+    buttonType === 'primary' ? '#eb9b00' : '#fff'};
   padding: 16px 32px;
   border: 2px solid #eb9b00;
-  color: #fff;
+  color: ${({ buttonType }) => (buttonType === 'primary' ? '#fff' : '#eb9b00')};
   font-size: 20px;
   cursor: pointer;
-  &:hover {
-    background: #b87900;
-    border: 2px solid #b87900;
-  }
+  ${({ buttonType }) =>
+    buttonType === 'primary'
+      ? css`
+          &:hover {
+            background: #b87900;
+            border: 2px solid #b87900;
+          }
+        `
+      : css`
+          &:hover {
+            background: #fff;
+            border: 2px solid #b87900;
+            color: #b87900;
+          }
+        `}
 `;
--- stories/AbButton.stories.tsx
+++ stories/AbButton.stories.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { AbButton } from '../src/components/AbButton';
+import { AbButton, AbButtonProps } from '../src/components/AbButton';
 
 export default {
   title: 'Components/AbButton',
   component: AbButton,
 } as ComponentMeta<typeof AbButton>;
 
-const Template: ComponentStory<typeof AbButton> = () => <AbButton />;
+const Template: ComponentStory<typeof AbButton> = args => (
+  <AbButton {...args} />
+);
 
 export const Primary = Template.bind({});
+
+Primary.args = {
+  text: 'Primary Button',
+  buttonType: 'primary',
+} as AbButtonProps;
+
+export const Secondary = Template.bind({});
+
+Secondary.args = {
+  text: 'Secondary Button',
+  buttonType: 'secondary',
+} as AbButtonProps;
